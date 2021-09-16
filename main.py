b094ecbba0be67e071d22d674c68e52bd48d83c7
Andris Jaunzemis <adjaunzemis@gmail.com>
2021-09-16 08:54:06 -0400

    Added example routes with query parameters

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from typing import Optional
 
 from fastapi import FastAPI
 
@@ -7,15 +8,31 @@ class ModelName(str, Enum):
     resnet = "resnet"
     lenet = "lenet"
 
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
+
 @app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
+async def read_item(item_id: int, q: Optional[str] = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update({"description": "This is an amazing item that has a long description!"})
+    return item
+
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(user_id: int, item_id: str, needy: str, skip: int = 0, limit: Optional[int] = None):
+    item = {"user_id": user_id, "item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
+    return item
 
 @app.get("/users/me")
 async def read_user_me():
